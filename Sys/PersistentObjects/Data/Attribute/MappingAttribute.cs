f01c95b477af3f1b3ccd056c2f6fbcb631c3702c
F Jiang <fjiang2@live.com>
2012-07-27 11:47:38 -0500

    Continue working on Mapping

--- Sys/PersistentObjects/Data/Attribute/MappingAttribute.cs
+++ Sys/PersistentObjects/Data/Attribute/MappingAttribute.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Data;
 using Tie;
+using System.Reflection;
 
 namespace Sys.Data
 {
@@ -73,10 +74,7 @@ namespace Sys.Data
         public readonly string Many2;       //Roles.ID
         public readonly string Relation2;   //UserRoles.Role_ID
         
-        public readonly Type RelationDpo;      //typeof(UserRoles)
-
-        private SqlClause clause1;           //A := SELECT UserRoles.Role_ID FROM UserRoles WHERE UserRoles.User_ID=@[User.ID]
-        private SqlClause clause2;           //B := SELECT * FROM Roles WHERE Roles.Role_ID IN (A)
+        public readonly Type TRelation;      //typeof(UserRoles)
 
         public Mapping_Many_To_ManyAttribute(string many1, string many2, Type relationDpo, string relation1, string relation2)
         {
@@ -85,10 +83,13 @@ namespace Sys.Data
 
             this.Relation1 = relation1;
             this.Relation2 = relation2;
-            this.RelationDpo = relationDpo;
-
-            
+            this.TRelation = relationDpo;
         }
-
+        
+        public Mapping_Many_To_ManyAttribute(string many1, string many2, Type relationDpo)
+            : this(many1, many2, relationDpo, many1, many2)
+        { 
+        
+        }
     }
 }
