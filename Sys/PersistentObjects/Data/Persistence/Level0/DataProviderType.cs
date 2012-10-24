0183342972b7356cd5f508b31de5cb6dc83d4789
fjiang <fjiang@TMB-FJIANG-L.hmt.pri>
2012-10-24 10:27:39 -0500

    Create class DataProvider which stands for Database Server to support multiple servers

---
+++ Sys/PersistentObjects/Data/Persistence/Level0/DataProviderType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sys.Data
+{
+    public enum DataProviderType
+    {
+        SqlServer,
+        Excel2007,
+        Excel2010,
+    }
+
+}
