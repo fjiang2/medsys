fa3a810fd31ca6393874dcf6f170fa9615a29d72
fjiang2 <fjiang2@localhost>
2012-11-14 11:28:31 -0600

    let DPO provider be integer, refer to [sys00704].handle

--- Sys/PersistentObjects/Data/Attribute/TableAttribute.cs
+++ Sys/PersistentObjects/Data/Attribute/TableAttribute.cs
@@ -9,25 +9,18 @@ namespace Sys.Data
     {
         private string tableName;
         private Level level;
-        private Provider provider;
+        public int Provider = (int)DataProvider.DEFAULT_HANDLE;
 
         public bool DefaultValueUsed = false;
         public bool Pack = true;
        
 
         public TableAttribute(string tableName, Level level)
-            :this(tableName, level, Provider.DefaultDataSource)
         {
             this.tableName = tableName;
             this.level = level;
         }
 
-        public TableAttribute(string tableName, Level level, Provider provider)
-        {
-            this.tableName = tableName;
-            this.level = level;
-            this.provider = provider;
-        }
 
         public Level Level
         {
@@ -39,7 +32,7 @@ namespace Sys.Data
             get 
             {
                 TableName tname;
-                DataProvider dataProvider = new DataProvider((int)this.provider);
+                DataProvider dataProvider = new DataProvider(this.Provider);
                 switch (this.Level)
                 {
                     case Level.System:
