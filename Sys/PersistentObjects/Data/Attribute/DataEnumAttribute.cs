e2c12519362905892c3e0779259940f3d3944060
fjiang2 <fjiang2@localhost>
2012-12-14 09:21:15 -0600

    add some comment

--- Sys/PersistentObjects/Data/Attribute/DataEnumAttribute.cs
+++ Sys/PersistentObjects/Data/Attribute/DataEnumAttribute.cs
@@ -5,13 +5,37 @@ using Tie;
 
 namespace Sys.Data
 {
+    /// <summary>
+    /// used for user defined data enum type from database
+    /// </summary>
     [System.AttributeUsage(System.AttributeTargets.Enum)]
     public class DataEnumAttribute : Attribute
     {
+        /// <summary>
+        /// description of DataEnum
+        /// </summary>
+        public readonly string Caption;
+        
+        /// <summary>
+        /// no description
+        /// </summary>
         public DataEnumAttribute()
         {
         }
 
+        /// <summary>
+        /// constuct by description
+        /// </summary>
+        /// <param name="caption"></param>
+        public DataEnumAttribute(string caption)
+        {
+            this.Caption = caption;
+        }
+
+        /// <summary>
+        /// return name of DataEnum Attribute
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             string attribute = typeof(DataEnumAttribute).Name.Replace("Attribute", "");
--- Sys/PersistentObjects/Data/Attribute/FieldAttribute.cs
+++ Sys/PersistentObjects/Data/Attribute/FieldAttribute.cs
@@ -5,17 +5,31 @@ using Tie;
 
 namespace Sys.Data
 {
+    /// <summary>
+    /// describe a field in class or enum
+    /// </summary>
     [System.AttributeUsage(System.AttributeTargets.Field)]
     public class FieldAttribute : Attribute
     {
+        /// <summary>
+        /// description of field
+        /// </summary>
         public string Caption;
 
-
+        /// <summary>
+        /// construct by description
+        /// </summary>
+        /// <param name="caption"></param>
         public FieldAttribute(string caption)
         {
             this.Caption = caption;
         }
 
+
+        /// <summary>
+        /// return attribute description
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             string attribute = typeof(FieldAttribute).Name.Replace("Attribute", "");
