a4025d4a89e4db885f96adee9802766f041e12f6
fjiang2 <fjiang2@localhost>
2013-01-07 10:43:33 -0600

    Make NTreeView generic to support Dpo List

---
+++ Sys/ViewManager/Forms/TreeDpoView/INTreeDpoNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Sys.ViewManager.Forms
+{
+    public interface INTreeDpoNode : ITreeDpoNode
+    {
+        Image IconImage { get; }
+        string Expression { get; }
+    }
+}
