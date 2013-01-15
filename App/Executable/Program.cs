ea5a4b0b39bbd7a43d10f04d4f72d0fa20f7d419
fjiang2 <fjiang2@localhost>
2013-01-15 15:33:04 -0600

    add internal static variable BaseForm.IMainForm Mainform;

--- App/Executable/Program.cs
+++ App/Executable/Program.cs
@@ -12,8 +12,13 @@ using Tie;
 using Sys;
 using Sys.Platform.Forms;
 
+
+
 namespace App.Executable
 {
+    /// <summary>
+    /// make Application target platform be .NET Framework 4.0, rather than .NET Framkework 4.0 Client Profile when Sys.Platform cannot be loaded
+    /// </summary>
     static class Program
     {
         const string applicationName = "Median System";
