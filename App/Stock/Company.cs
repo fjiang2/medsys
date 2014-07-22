ed2f992027929d646e30832c15d5be648fad832f
Frank Jiang <Frank Jiang@localhost>
2014-07-22 02:12:00 -0500

    Save html locally

--- App/Stock/Company.cs
+++ App/Stock/Company.cs
@@ -12,25 +12,55 @@ namespace Stock
 {
     public class Company
     {
+        protected const string LOG_FOLDER = "C:\\log";
         const string searchCompanyFormat = "http://www.sec.gov/cgi-bin/browse-edgar?CIK={0}&Find=Search&owner=exclude&action=getcompany";
         const string getInsiderTransactionFormat = "http://www.sec.gov/cgi-bin/own-disp?action=getissuer&CIK={0}";
 
         WebClient client = new WebClient();
         private string symbol;
 
-        public string CompanyName { get; set; }
-        public string CIK { get; set; }
+        public string CompanyName { get; private set; }
+        public string CIK { get; private set; }
+        public bool HasInsiderTransactions { get; private set; }
 
         public DataTable Fillings { get; private set; }
         public DataTable Ownerships { get; private set; }
         public DataTable Transactions { get; private set; }
 
-        public Company(string symbol, bool hasInsiderTransaction)
+        public Company()
+        {
+        }
+        
+        public void Download(string symbol)
         {
             this.symbol = symbol;
+
+            GetCompanyInfo();
+
+            //if (this.HasInsiderTransactions)
+            //    GetInsiderTransactions(this.CIK);
         }
 
-        public void Download()
+        public void Download(string symbol, string CIK, bool hasInsiderTransaction)
+        {
+            this.symbol = symbol;
+            this.CIK = CIK;
+
+            if (hasInsiderTransaction)
+            {
+                GetInsiderTransactions(this.CIK);
+                this.HasInsiderTransactions = true;
+            }
+            else
+            {
+                GetCompanyInfo();
+
+                if (this.HasInsiderTransactions)
+                    GetInsiderTransactions(this.CIK);
+            }
+        }
+
+        private void GetCompanyInfo()
         {
             Uri uri = new Uri(string.Format(searchCompanyFormat, symbol));
             string html = client.DownloadString(uri);
@@ -41,9 +71,8 @@ namespace Stock
             this.CompanyName = parser.CompanyName;
             this.CIK = parser.CIK;
             this.Fillings = parser.filling;
-
-            if (parser.HasInsiderTransactions)
-                GetInsiderTransactions(this.CIK);
+            this.HasInsiderTransactions = parser.HasInsiderTransactions;
+        
         }
 
         private void GetInsiderTransactions(string cik)
@@ -51,11 +80,26 @@ namespace Stock
             Uri uri = new Uri(string.Format(getInsiderTransactionFormat, cik));
             string html = client.DownloadString(uri);
 
+            SaveHtml(string.Format("{0}\\InsiderTransactions\\{1}\\{2}.html", LOG_FOLDER, DateTime.Today.ToString("yyyy-mm-dd"), symbol), html);
+
+
             var parser = new InsiderTransactionHtml(html, HtmlSource.Web);
             parser.ParseHtml();
 
             this.Ownerships = parser.ownership;
             this.Transactions = parser.transaction;
+        }
+
+        public static void SaveHtml(string path, string html)
+        {
+            string folder = Path.GetDirectoryName(path);
+            if (!Directory.Exists(folder))
+                Directory.CreateDirectory(folder);
+
+            using (var writer = new StreamWriter(path))
+            {
+                writer.Write(html);
+            }
         }
     }
 }
--- App/Stock/DailyFetch.cs
+++ App/Stock/DailyFetch.cs
@@ -7,6 +7,7 @@ using System.Data;
 using System.IO;
 using System.Net;
 using Sys.Data;
+using Stock.DpoClass;
 
 namespace Stock
 {
@@ -15,8 +16,84 @@ namespace Stock
   
         public DailyFetch()
         {
-            Company company = new Company("AIR", false);
-            company.Download();
+            //InitilizeCompanies();
+            FetchTransactions();
+
+        }
+
+
+        private void FetchTransactions()
+        {
+            TableReader<CompanyDpo> reader = new TableReader<CompanyDpo>(CompanyDpo._Has_Insider_Transaction.ColumnName() == 1);
+
+            foreach (DataRow row in reader.Table.Rows)
+            {
+                CompanyDpo dpo = new CompanyDpo(row);
+                
+                Company company = new Company();
+                company.Download(dpo.Symbol, dpo.CIK, dpo.Has_Insider_Transaction);
+                dpo.Has_Insider_Transaction = company.HasInsiderTransactions;
+
+                dpo.Last_Updated_Time = DateTime.Now;
+                dpo.Save();
+
+                foreach (DataRow row1 in company.Ownerships.Rows)
+                {
+                    OwnershipDpo dpo1 = new OwnershipDpo(row1);
+                    dpo1.Save();
+                }
+
+                foreach (DataRow row2 in company.Transactions.Rows)
+                {
+                    TransactionDpo dpo2 = new TransactionDpo(row2);
+                    dpo2.Save();
+                }
+
+            }
+
+        }
+
+
+        /// <summary>
+        /// Initialize Table [Companies] in SQL Server, Run once.
+        /// </summary>
+        /// <param name="table"></param>
+        private void InitilizeCompanies()
+        {
+            TableReader<CompanyDpo> reader = new TableReader<CompanyDpo>();
+
+
+            foreach (DataRow row in reader.Table.Rows)
+            {
+                CompanyDpo dpo = new CompanyDpo(row);
+                if (dpo.Inactive)
+                    continue;
+
+                Company company = new Company();
+
+                if (dpo.CIK == null)
+                {
+                    try
+                    {
+                        company.Download(dpo.Symbol);
+                        dpo.CIK = company.CIK;
+                        dpo.Has_Insider_Transaction = company.HasInsiderTransactions;
+                    }
+                    catch (Exception)
+                    {
+                        dpo.Inactive = true;
+                    }
+                }
+                else
+                {
+                    //company.Download(dpo.CIK, dpo.Has_Insider_Transaction);
+                    //dpo.Has_Insider_Transaction = company.HasInsiderTransactions;
+                }
+
+                dpo.Last_Updated_Time = DateTime.Now;
+                dpo.Save();
+
+            }
         }
 
       
