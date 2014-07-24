a8e9ff4d4353fdcd6763e5f988e869e20844de29
Frank Jiang <Frank Jiang@localhost>
2014-07-23 23:32:29 -0500

    Add LoadHtmlForm and ParseHtmlForm

---
+++ App/Stock/Forms/LoadHtmlForm.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Stock.Forms
+{
+    partial class LoadHtmlForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.txtSymbol = new System.Windows.Forms.TextBox();
+            this.txtPercentage = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Symbol:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(23, 73);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(512, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(289, 163);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(374, 163);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(459, 163);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtSymbol
+            // 
+            this.txtSymbol.Location = new System.Drawing.Point(70, 42);
+            this.txtSymbol.Name = "txtSymbol";
+            this.txtSymbol.ReadOnly = true;
+            this.txtSymbol.Size = new System.Drawing.Size(145, 20);
+            this.txtSymbol.TabIndex = 4;
+            // 
+            // txtPercentage
+            // 
+            this.txtPercentage.Location = new System.Drawing.Point(359, 42);
+            this.txtPercentage.Name = "txtPercentage";
+            this.txtPercentage.ReadOnly = true;
+            this.txtPercentage.Size = new System.Drawing.Size(102, 20);
+            this.txtPercentage.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(467, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Downloaded";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(20, 127);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(38, 13);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "Ready";
+            // 
+            // LoadHtmlForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 198);
+            this.Controls.Add(this.txtPercentage);
+            this.Controls.Add(this.txtSymbol);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "LoadHtmlForm";
+            this.Text = "Download web pages from Edgar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnClose;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox txtSymbol;
+        private System.Windows.Forms.TextBox txtPercentage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
---
+++ App/Stock/Forms/LoadHtmlForm.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sys.Data;
+using Sys.ViewManager.Forms;
+using Sys.BusinessRules;
+using Stock.DpoClass;
+
+
+namespace Stock.Forms
+{
+    public partial class LoadHtmlForm : BaseForm
+    {
+        private DailyFetch dailyFetch = new DailyFetch();
+
+        public LoadHtmlForm()
+        {
+            InitializeComponent();
+
+            this.Load += LoadHtmlForm_Load;
+            
+            backgroundWorker1.DoWork += backgroundWorker1_DoWork;
+            backgroundWorker1.ProgressChanged += backgroundWorker1_ProgressChanged;
+            backgroundWorker1.RunWorkerCompleted += backgroundWorker1_RunWorkerCompleted;
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+
+            this.btnStart.Enabled = true;
+            this.btnStop.Enabled = false;
+            this.btnClose.Enabled = true;
+        }
+
+        void LoadHtmlForm_Load(object sender, EventArgs e)
+        {
+            dailyFetch.ReadCompanies();
+            progressBar1.Minimum = 1;
+            progressBar1.Maximum = Count;
+
+        }
+
+        public override void RuleDefinition(ValidateProvider provider)
+        {
+        }
+
+        private int Count
+        {
+            get
+            {
+                return dailyFetch.CompanyTable.Rows.Count;
+            }
+        }
+
+       
+        void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            int i = 1;
+            foreach (DataRow row in dailyFetch.CompanyTable.Rows)
+            {
+                if ((worker.CancellationPending == true))
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    CompanyDpo dpo = new CompanyDpo(row);
+                    worker.ReportProgress((int)(100.0 * i / Count), new UserState { ith = i, Symbol = dpo.Symbol });
+
+                    Company company = new Company();
+                    if(dpo.Has_Insider_Transaction)
+                        company.DownloadHtml(dpo.Symbol, dpo.CIK);
+
+                    dpo.Last_Updated_Time = DateTime.Now;
+                    dpo.Save();
+
+                    i++;
+
+                }
+            }
+
+        }
+
+        void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            UserState state = (UserState)e.UserState;
+            int ith =state.ith;
+            txtSymbol.Text = state.Symbol;
+
+            progressBar1.Value = ith;
+            txtPercentage.Text = string.Format("{0}/{1}", ith, Count);
+        }
+
+        void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if ((e.Cancelled == true))
+            {
+                this.lblStatus.Text = "Download Canceled!";
+            }
+
+            else if (!(e.Error == null))
+            {
+                this.lblStatus.Text = ("Error: " + e.Error.Message);
+            }
+
+            else
+            {
+                this.lblStatus.Text = "Download Completed."; 
+            }
+
+          
+        }
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+
+            this.btnStart.Enabled = false;
+            this.btnStop.Enabled = true;
+            this.btnClose.Enabled = false;
+
+            this.lblStatus.Text = "Downloading...";
+        }
+
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.WorkerSupportsCancellation == true)
+            {
+                backgroundWorker1.CancelAsync();
+
+                this.btnStart.Enabled = true;
+                this.btnStop.Enabled = false;
+                this.btnClose.Enabled = true;
+
+                this.lblStatus.Text = "Download is stopped";
+            }
+            else
+                this.lblStatus.Text = "Cannot stop this moment.";
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+    class UserState
+    {
+        public int ith { get; set; }
+        public string Symbol { get; set; }
+    }
+}
