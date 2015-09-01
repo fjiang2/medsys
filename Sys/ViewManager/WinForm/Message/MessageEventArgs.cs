61980ba5a22392f677eebfa8fdae5aac1b4e1cc6
Frank Jiang <fjiang2@hotmail.com>
2015-08-31 21:50:11 -0500

    removed MessageEventArgs

--- Sys/ViewManager/WinForm/Message/MessageEventArgs.cs
+++ Sys/ViewManager/WinForm/Message/MessageEventArgs.cs
@@ -8,16 +8,6 @@ namespace Sys.ViewManager.Forms
     public delegate void MessageHandler(object sender, MessageEventArgs e);
     public delegate void MessagePlaceHandler(object sender, MessagePlaceEventArgs e);
 
-    public class MessageEventArgs : EventArgs
-    {
-        public readonly Message Message;
-
-        public MessageEventArgs(Message message)
-        {
-            this.Message = message;
-        }
-    }
-
     public class MessagePlaceEventArgs : EventArgs
     {
         public readonly MessagePlace Place;
